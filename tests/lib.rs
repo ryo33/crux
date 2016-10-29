68471b07027dc40937e37a47ca1851edcc3215b7
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-10-30 01:04:48 +0900

    Modify the test

--- tests/lib.rs
+++ tests/lib.rs
@@ -6,9 +6,10 @@ use rstate::Store;
 use rstate::Middleware;
 
 enum TestAction {
+    Add(i32),
     Increment,
     Decrement,
-    Add(i32),
+    BonusTime, // 20ms Bonus Time
 }
 
 struct TestState {
@@ -23,6 +24,7 @@ impl State for TestState {
             TestAction::Add(x) => self.number += x,
             TestAction::Increment => self.number += 1,
             TestAction::Decrement => self.number -= 1,
+            _ => {},
         }
     }
 }
@@ -33,22 +35,26 @@ struct TestMiddleware {
 
 impl Middleware<TestState> for TestMiddleware {
     fn dispatch(&mut self, store: Store<TestState, Self>, next: &Fn(TestAction), action: TestAction) {
-        let mut store_mut = store.clone();
-        let counter = self.counter;
         match action {
-            TestAction::Add(_) => (),
-            _ => {
+            TestAction::BonusTime => {
+                self.counter += 1;
+
+                let mut store_mut = store.clone();
+                let counter = self.counter;
+
                 thread::spawn(move || {
-                    thread::sleep(Duration::from_millis(5));
                     let number = store_mut.state().number;
-                    thread::sleep(Duration::from_millis(15));
+                    thread::sleep(Duration::from_millis(20));
                     let next_number = store_mut.state().number;
-                    store_mut.dispatch(TestAction::Add(next_number - number + counter));
+                    let bonus = (next_number - number) * counter;
+                    store_mut.dispatch(TestAction::Add(bonus));
                 });
-            }
+                thread::sleep(Duration::from_millis(1));
+            },
+            _ => {
+                next(action);
+            },
         }
-        self.counter += 1;
-        next(action);
     }
 }
 
@@ -62,36 +68,37 @@ fn store() {
     };
     let mut store = Store::new(state, middleware);
 
-    {
-        let current_state = store.state();
-        assert_eq!(current_state.number, 0);
-    }
+    assert_eq!(store.state().number, 0);
 
     store.dispatch(TestAction::Increment);
-    // counter = 0
     assert_eq!(store.state().number, 1);
 
-    thread::sleep(Duration::from_millis(10));
-
     store.dispatch(TestAction::Add(2));
-    // counter = 1
     assert_eq!(store.state().number, 3);
 
-    thread::sleep(Duration::from_millis(15));
-    // counter = 2
-    assert_eq!(store.state().number, 3 + (3 - 1) + 0); // 5
-    // counter = 3
-    thread::sleep(Duration::from_millis(15));
-    // counter = 4
-    assert_eq!(store.state().number, 5);
+    // start BonusTime
+    store.dispatch(TestAction::BonusTime);
+
+    store.dispatch(TestAction::Add(3));
+    assert_eq!(store.state().number, 6);
 
     store.dispatch(TestAction::Decrement);
-    // counter = 5
-    assert_eq!(store.state().number, 4);
-    store.dispatch(TestAction::Add(-2));
-    // counter = 6
-    assert_eq!(store.state().number, 2);
+    assert_eq!(store.state().number, 5);
 
+    // finish BonusTime
     thread::sleep(Duration::from_millis(25));
-    assert_eq!(store.state().number, 2 + (2 - 4) + 5);
+    assert_eq!(store.state().number, 5 + (5 - 3) * 1); // 7
+
+    // start BonusTime
+    store.dispatch(TestAction::BonusTime);
+
+    store.dispatch(TestAction::Add(-4));
+    assert_eq!(store.state().number, 3);
+
+    store.dispatch(TestAction::Increment);
+    assert_eq!(store.state().number, 4);
+
+    // finish BonusTime
+    thread::sleep(Duration::from_millis(25));
+    assert_eq!(store.state().number, 4 + (4 - 7) * 2); // -1
 }
